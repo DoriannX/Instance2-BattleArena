571322729b4e88714398fb7ec0671f2b3771fecb
Doriann <150040299+DoriannX@users.noreply.github.com>
2025-01-29 17:32:01 +0100

    fixing online

--- Instance2-BattleArena/Assets/Scripts/Managers/RelayManager.cs
+++ Instance2-BattleArena/Assets/Scripts/Managers/RelayManager.cs
@@ -1,64 +1,16 @@
-using System.Threading.Tasks;
 using Unity.Netcode;
-using Unity.Netcode.Transports.UTP;
-using Unity.Networking.Transport.Relay;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
-using Unity.Services.Relay;
-using Unity.Services.Relay.Models;
 using UnityEngine;
 
 namespace Managers
 {
-    public class RelayManager: MonoBehaviour
+    public class RelayManager : MonoBehaviour
     {
-        private async void Start()
+        private void Awake()
         {
-            await UnityServices.InitializeAsync();
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-        }
-
-        public async Task JoinRandomRelay()
-        {
-            await JoinRelay(RoomManager.GetRandomCode());
-        }
-        
-        public async Task JoinRelay(string joinCode)
-        {
-            try
+            if (Application.platform == RuntimePlatform.WindowsServer || Application.platform == RuntimePlatform.LinuxServer)
             {
-                JoinAllocation allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-
-                UnityTransport relayTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
-                relayTransport.SetRelayServerData(new RelayServerData(allocation, "dtls"));
-
-                NetworkManager.Singleton.StartClient();
-            }
-            catch (RelayServiceException e)
-            {
-                Debug.LogError(e);
-            }
-        }
-        
-        public async Task<string> CreateRelay()
-        {
-            try
-            {
-                Allocation allocation = await RelayService.Instance.CreateAllocationAsync(4);
-                string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
-
-                UnityTransport relayTransport = NetworkManager.Singleton.GetComponent<UnityTransport>();
-                relayTransport.SetRelayServerData(new RelayServerData(allocation, "dtls"));
-
-                NetworkManager.Singleton.StartHost();
-                RoomManager.AddRoomCode(joinCode);
-
-                return joinCode;
-            }
-            catch (RelayServiceException e)
-            {
-                Debug.LogError(e);
-                return null;
+                NetworkManager.Singleton.StartServer();
+                Debug.Log("started server");
             }
         }
     }
--- Instance2-BattleArena/Assets/Scripts/UI/NetworkUIManager.cs
+++ Instance2-BattleArena/Assets/Scripts/UI/NetworkUIManager.cs
@@ -1,44 +1,78 @@
-using System;
-using Managers;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
+using TMPro;
 
 namespace UI
 {
     [RequireComponent(typeof(NetworkManager))]
     public class NetworkUIManager : MonoBehaviour
     {
-        [SerializeField] private Button _hostBtn, _clientBtn;
+        [SerializeField] private Button _clientBtn;
+        [SerializeField] private TMP_InputField _roomIpAddressInput;
         private NetworkManager _networkManager;
+        private UnityTransport _unityTransport;
 
         private void Awake()
         {
             _networkManager = NetworkManager.Singleton;
+            Assert.IsNotNull(_clientBtn, "Client button is not assigned");
+            Assert.IsNotNull(_roomIpAddressInput, "Room ip address input is not assigned");
+            _unityTransport = _networkManager.GetComponent<UnityTransport>();
         }
 
         private void ToggleBtns(bool state)
         {
-            _hostBtn.gameObject.SetActive(state);
             _clientBtn.gameObject.SetActive(state);
+            _roomIpAddressInput.gameObject.SetActive(state);
         }
 
         private void Start()
         {
-            _hostBtn.onClick.AddListener(OnHostBtnClicked);
             _clientBtn.onClick.AddListener(OnClientBtnClicked);
+        }
+
+        private string OnRoomAddressEntered()
+        {
+            return _unityTransport.ConnectionData.Address = _roomIpAddressInput.text;
         }
 
         private void OnClientBtnClicked()
         {
+            if (!IsValidIpAddress(OnRoomAddressEntered()))
+            {
+                Debug.LogError("invalid ip address");
+                return;
+            }
+            
             _networkManager.StartClient();
             ToggleBtns(false);
         }
 
-        private void OnHostBtnClicked()
+        private bool IsValidIpAddress(string ipAddress)
         {
-            _networkManager.StartHost();
-            ToggleBtns(false);
+            if (string.IsNullOrWhiteSpace(ipAddress))
+            {
+                return false;
+            }
+
+            string[] splitValues = ipAddress.Split('.');
+            if (splitValues.Length != 4)
+            {
+                return false;
+            }
+
+            foreach (string item in splitValues)
+            {
+                if (!int.TryParse(item, out int tempValue) || tempValue < 0 || tempValue > 255)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
